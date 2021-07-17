8942faa2dd3132061060e9b2fba5b88cc40e355d
Leandro Alves <leandro.job@live.com>
2021-07-17 17:44:51 -0300

    Faktory useCase

--- application/faktory/faktory.go
+++ application/faktory/faktory.go
@@ -1,11 +1,17 @@
 package faktory
 
-import "github.com/cogny/go_verdao/application/model"
+import (
+	"github.com/cogny/go_verdao/application/model"
+	"github.com/cogny/go_verdao/application/usecase"
+	"github.com/cogny/go_verdao/infrastructure/repository"
+	"github.com/jinzhu/gorm"
+)
 
-func TestAPIUseCaseFaktory(uris []model.URI) model.Config {
-	config, err := model.LoadConfig("config.json")
-	if err != nil {
-		panic(err)
+func TestAPIUseCaseFaktory(uris []model.URI, database *gorm.DB) usecase.TestAPIUseCase {
+	repo := repository.ResultRespositoryDB{DB: database}
+	testAPIUseCase := usecase.TestAPIUseCase{
+		APIs:             uris,
+		ResultRepository: repo,
 	}
-	return config
+	return testAPIUseCase
 }
