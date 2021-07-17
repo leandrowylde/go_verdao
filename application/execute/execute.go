4d135aecef18a7baa5cb52f5b70213e93426f842
Leandro Alves <leandro.job@live.com>
2021-07-17 17:44:41 -0300

    Execute api testing loop

--- application/execute/execute.go
+++ application/execute/execute.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/cogny/go_verdao/application/faktory"
 	"github.com/cogny/go_verdao/application/model"
 	"github.com/cogny/go_verdao/application/usecase"
+	"github.com/jinzhu/gorm"
 )
 
 type Execute struct {
@@ -13,20 +15,80 @@ type Execute struct {
 }
 
 func (e *Execute) Start(config model.Config) {
-	ticker := time.NewTicker(5 * time.Second)
+	tickerStart := time.NewTicker(5 * time.Second)
+	tickerStop := time.NewTicker(5 * time.Second)
 	done := make(chan bool)
+
+	fmt.Printf("Vai verdão (Agora o mundial vem)...\n")
+
+	execRequest := func(uri model.URI) {
+		code, data, err := e.TestAPIUseCase.MakeRequest(uri)
+		if err != nil {
+			panic(err)
+		}
+		var logData string
+		if len(data) > 101 {
+			logData = data[0:100] + "..."
+		} else {
+			logData = data
+		}
+		fmt.Printf("Call API: %v - %v", code, logData)
+	}
 
 	go func() {
 		for {
 			select {
 			case <-done:
 				return
-			case t := <-ticker.C:
-				fmt.Printf("Check schedule...: %v", t)
-				fmt.Printf("Start: %v", config.Start)
-				fmt.Printf("Stop: %v", config.Stop)
-				fmt.Printf("URIs: %v", config.URLs)
+			case <-tickerStart.C:
+				timeStart, err := time.Parse(time.RFC3339, config.Start)
+				if err != nil {
+					panic(err)
+				}
+				timeStop, err := time.Parse(time.RFC3339, config.Stop)
+				if err != nil {
+					panic(err)
+				}
+				fmt.Printf("Config start time: %v | Time Now: %v\n", timeStart, time.Now().Format(time.RFC3339))
+				if time.Now().After(timeStart) && time.Now().Before(timeStop) {
+					fmt.Printf("GAME TIME\n")
+					for _, u := range e.TestAPIUseCase.APIs {
+						go execRequest(u)
+					}
+				}
 			}
 		}
 	}()
+
+	go func() {
+		for {
+			select {
+			case <-done:
+				return
+			case <-tickerStop.C:
+				timeStop, err := time.Parse(time.RFC3339, config.Stop)
+				if err != nil {
+					panic(err)
+				}
+				fmt.Printf("Config stop time: %v | Time Now: %v\n", timeStop, time.Now().Format(time.RFC3339))
+				if time.Now().After(timeStop) {
+					fmt.Printf("END GAME...sending stop ticker\n")
+					done <- true
+				}
+			}
+		}
+	}()
+	<-done
+}
+
+func NewExecuteProcess(database *gorm.DB) (Execute, error) {
+	config, err := model.LoadConfig("config.json")
+	if err != nil {
+		panic(err)
+	}
+	testAPIsUseCase := faktory.TestAPIUseCaseFaktory(config.URLs, database)
+
+	return Execute{
+		TestAPIUseCase: testAPIsUseCase,
+	}, nil
 }
