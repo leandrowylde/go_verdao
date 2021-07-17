4a93e0b92dd789ef4f7b686399006a41f463c8d3
Leandro Alves <leandro.job@live.com>
2021-07-17 17:45:32 -0300

    Update URI type to string in db

--- application/model/result.go
+++ application/model/result.go
@@ -4,7 +4,7 @@ import "time"
 
 type Result struct {
 	CreatedAt    time.Time    `json:"created_at"`
-	RequestURI   URI          `json:"uri" gorm:"column:uri;type:varchar(255);not null"`
+	RequestURI   string       `json:"uri" gorm:"column:uri;type:varchar"`
 	ResponseCode ResponseCode `json:"code" gorm:"column:code;type:int"`
 	ResponseData string       `json:"data" gorm:"column:data;type:varchar"`
 }
@@ -16,7 +16,7 @@ type ResultRepositoryInterface interface {
 func NewResult(uri URI, code ResponseCode, data string) (*Result, error) {
 	result := Result{
 		CreatedAt:    time.Now(),
-		RequestURI:   uri,
+		RequestURI:   uri.URL,
 		ResponseCode: code,
 		ResponseData: data,
 	}
